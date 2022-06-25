d611654ca11ded5fda96889104ca11309b7456df
Yura52 <strausmg@gmail.com>
2022-06-25 12:37:14 +0300

    data | add functions for building bins

--- rtdl/data.py
+++ rtdl/data.py
@@ -1,8 +1,73 @@
 """Tools for data (pre)processing."""
 
-from typing import List
+import warnings
+from typing import Any, Dict, List
 
 import numpy as np
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+
+
+def _adjust_bin_counts(X: np.ndarray, n_bins: int) -> List[int]:
+    if n_bins < 2:
+        raise ValueError('n_bins must be greater than 1')
+    adjusted_bin_counts = []
+    for i, column in enumerate(X.T):
+        n_unique = len(np.unique(column))
+        if n_unique < 2:
+            raise ValueError(f'All elements in the column {i} are the same')
+        if n_unique < n_bins:
+            warnings.warn(
+                f'For the feature {i}, the number of bins will be set to the number of'
+                ' distinct values, becuase the provided n_bins is greater than this number.'
+            )
+        adjusted_bin_counts.append(min(n_bins, n_unique))
+    return adjusted_bin_counts
+
+
+def build_quantile_bins(X: np.ndarray, n_bins: int) -> List[np.ndarray]:
+    adjusted_bin_counts = _adjust_bin_counts(X, n_bins)
+    edges = []
+    for column, adjusted_n_bins in zip(X.T, adjusted_bin_counts):
+        # quantiles include 0.0 and 1.0
+        quantiles = np.linspace(0.0, 1.0, adjusted_n_bins + 1)
+        edges.append(np.unique(np.quantile(column, quantiles)))
+    return edges
+
+
+def build_decision_tree_bins(
+    X: np.ndarray,
+    n_bins: int,
+    *,
+    y: np.ndarray,
+    regression: bool,
+    tree_kwargs: Dict[str, Any],
+) -> List[np.ndarray]:
+    if len(X) != len(y):
+        raise ValueError('X and y have different first dimensions')
+    if 'max_leaf_nodes' in tree_kwargs:
+        raise ValueError(
+            'Do not include max_leaf_nodes in tree_kwargs (it will be set equal to n_bins automatically).'
+        )
+    adjusted_bin_counts = _adjust_bin_counts(X, n_bins)
+    edges = []
+    for column, adjusted_n_bins in zip(X.T, adjusted_bin_counts):
+        tree = (
+            (DecisionTreeRegressor if regression else DecisionTreeClassifier)(
+                max_leaf_nodes=adjusted_n_bins, **tree_kwargs
+            )
+            .fit(column.reshape(-1, 1), y)
+            .tree_
+        )
+        tree_thresholds = []
+        for node_id in range(tree.node_count):
+            # the following condition is True only for split nodes
+            # See https://scikit-learn.org/stable/auto_examples/tree/plot_unveil_tree_structure.html#sphx-glr-auto-examples-tree-plot-unveil-tree-structure-py
+            if tree.children_left[node_id] != tree.children_right[node_id]:
+                tree_thresholds.append(tree.threshold[node_id])
+        tree_thresholds.append(column.min())
+        tree_thresholds.append(column.max())
+        edges.append(np.array(sorted(set(tree_thresholds))))
+    return edges
 
 
 def get_category_sizes(X: np.ndarray) -> List[int]:
